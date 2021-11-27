e904879ff950a307e2c19d46e30edbad82ae579a
koteswararao Madduru <kotsmaddhur@gmail.com>
2021-11-27 17:47:33 +0530

    final changes

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,39 +1,27 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
 
     // Write you code here
-    if (hasDrivingLiscence === true && isTired==false && isSober==false){
-        return "You shouldn't drive";
-    }
-    else if (hasDrivingLiscence=== true &&  isTired === true && isSober==false){
-        return "You shouldn't drive";
-    }
-    else if (hasDrivingLiscence===true&&  isTired === true && isSober==true){
-        return "You shouldn't drive";
-    }
-    else if (hasDrivingLiscence===true &&  isTired === false && isSober==true){
-        return "You can drive";
-    }
-    else if (hasDrivingLiscence=== false &&  isTired === true && isSober==false){
-        return "You cannot drive";
-    }
-    else if (hasDrivingLiscence=== false &&  isTired === true && isSober==true){
-        return "You cannot drive";
-    }
-    else if (hasDrivingLiscence===false&&  isTired === false && isSober==true){
-        return "You cannot drive";
-    }
-    else if (hasDrivingLiscence===false&&  isTired === false && isSober==false){
-        return "You cannot drive";
-    }
-    else if (hasDrivingLiscence=== true &&  isTired === false && isSober==true){
-        return "You can drive";
-    }
-   
-
-
-
+    if (hasDrivingLiscence === true){
+        if (isTired=== false){
+            if (isSober === true){
+                    return "You can drive";
+            }
+            else{
+                return "You shouldn't drive";
+            }
+        
     
-    
+        }           
+        else{
+            return "You shouldn't drive";
+        }
+    }
+    else {
+        return 'You cannot drive';
+    }
 }
 
+
 module.exports = CanDrive;
+
+
