ab9c771c381c52916d6bbdb7a373a11cb6b88f8f
koteswararao Madduru <kotsmaddhur@gmail.com>
2021-12-15 16:28:21 +0530

    todo with actual API submission

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,19 +1,82 @@
-var addTodoButton = document.querySelector("button");
-var input = document.querySelector("input")
-var TodoContainer =  document.querySelector("div.todos")
-let counter = 0;
-addTodoButton.addEventListener("click", function(){
-  
-  var paragraph = document.createElement('p');
-  paragraph.innerHTML = input.value;
-  paragraph.setAttribute('key',counter++);
-  TodoContainer.appendChild(paragraph);
-  
-  paragraph.addEventListener("click", function(){
-    TodoContainer.removeChild(paragraph);
+function AllTodos() {
+    var div = document.getElementsByClassName("Todos")[0];
+    div.innerHTML = '';
+    var h5 = document.createElement('h5');
+    h5.innerHTML = "All TODOS"
+    div.appendChild(h5);
+    
+    var ul = document.createElement("ul");
+    div.appendChild(ul);
+    
+    
+        async function fetchData(url) {
+    
+            const response = await fetch(url);
+            var data = await response.json();
+            for (let i = 0; i < data.length; i++) {
+                var li = document.createElement('li');
+                li.innerHTML = "userId  :" + data[i].userId + " id :" + data[i].id + "  title :" + data[i].title + "  completed : " + data[i].completed;
+                ul.append(li);
+            }
+    
+        }
+    
+        fetchData('https://jsonplaceholder.typicode.com/todos');
+    };
+    
+function Completed(){
+    var div = document.getElementsByClassName("Todos")[0];
+    div.innerHTML = '';
+    var h5 = document.createElement('h5');
+    h5.innerHTML = "COMPLETED TODOS"
+    div.appendChild(h5);
+    
+    var ul = document.createElement("ul");
+    div.appendChild(ul);
 
-  })
-  input.value = '';
+    async function fetchData(url) {
+    
+        const response = await fetch(url);
+        var data = await response.json();
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].completed){
+                var li = document.createElement('li');
+                li.innerHTML = "userId  :" + data[i].userId + " id :" + data[i].id + "  title :" + data[i].title + "  completed : " + data[i].completed;
+                ul.append(li);
 
+            }
+        }
+        
 
-})
+    }
+    fetchData('https://jsonplaceholder.typicode.com/todos');
+}
+
+function Pending(){
+    var div = document.getElementsByClassName("Todos")[0];
+    div.innerHTML = '';
+    var h5 = document.createElement('h5');
+    h5.innerHTML = "PENDING TODOS"
+    div.appendChild(h5);
+    
+    var ul = document.createElement("ul");
+    div.appendChild(ul);
+
+    async function fetchData(url) {
+    
+        const response = await fetch(url);
+        var data = await response.json();
+        for (let i = 0; i < data.length; i++) {
+            if(!data[i].completed){
+                var li = document.createElement('li');
+                li.innerHTML = "userId  :" + data[i].userId + " id :" + data[i].id + "  title :" + data[i].title + "  completed : " + data[i].completed;
+                ul.append(li);
+
+            }
+        }
+        
+
+    }
+    fetchData('https://jsonplaceholder.typicode.com/todos');
+}
+
